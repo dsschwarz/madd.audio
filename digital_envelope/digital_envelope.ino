3b1a446c83fa7a6b5d6e90f81f23399b2ae19f39
adam <adam@adamthompson.ca>
2017-03-12 19:07:29 -0400

    added test files and envlope modifications

--- digital_envelope/digital_envelope.ino
+++ digital_envelope/digital_envelope.ino
@@ -1,5 +1,8 @@
 // #define i/o pins
-#define MIC_IN 14 // Mic in is Pin 14
+#define GND 14 // Ground reference pin (for calibration)
+#define MIC_IN 15 // Mic in Pin
+#define LEFT_BIT_1 1
+#define LEFT_BIT_3 3
 
 const int STORED_PAST_DB = 8; // The number of records to store (overwrites oldest record)
 const int WINDOW = 500; // length of envelope window time in millis
@@ -13,17 +16,19 @@ double currentMaxDB = 0; // NOTE: this could also be stored in the array. Keepin
 int storedPastDB[STORED_PAST_DB];
 int currentIndex = 0; // the current record to write to
 int numRecords = 0; // the number of records. Values above STORED_PAST_DB are meaningless
+int gnd_ref = 0;
+
+int vinPrev = 10; // DEBUG VARIABLE (for input simulation)
 
 // microphone calibration
-// TODO update these values for the new circuit
-const double p0 = 0.00002; // Pa8
-const double sensitivity = 6.31; //mV/Pa
+const double p0 = 0.00002; // Reference Pressure
+const double sensitivity = 1000 * pow(10,(-53/20)); //in mV/Pa - convert the sensitivity in dBV/Pa to mV/Pa
 const double A_filt = 0.502; // A-filter gain
 //const double R1 = 9890; // Ohms
 const double R1 = 4658; // Ohms
 //const double R2 = 179500; //Ohms
-const double R2 = 1011000; //Ohms
-const double G = R2/R1; // Pre-amp gain
+const double R2 = 98600; //Ohms
+const double G = 1 + R2/R1; // Pre-amp gain
 const double S_total = p0*sensitivity*A_filt*G; //Overall Gain
 
 
@@ -34,71 +39,94 @@ int isClosed;
 
 
 void setup() {
+  Serial.begin(9600);
   lastResetTime = millis();
   Serial.print("Initializing with time stamp: ");
-  Serial.println(lastResetTime);
-
+//  Serial.println(lastResetTime);
   // for testing
   startTime = lastResetTime;
   isClosed = 0;
+  gnd_ref = analogRead(GND);
+  
 }
 
 void loop() {
-  double dB = V_to_dB(getInput()); // TODO: implement fake getInput() for testing, otherwise hook up to the old getInput method.
-  Serial.print("dB is: ");
+  double dB = V_to_dB(getTestInput()); // TODO: implement fake getInput() for testing, otherwise hook up to the old getInput method.
+//  Serial.print("dB is: ");
   Serial.println(dB);
   recordDB(dB);
 
-  if (shouldOpen()) {
-    openDevice();
-  } else if (shouldClose()) {
-    closeDevice();
-  }
+  testHBridge();
+
+//  if (shouldOpen()) {
+//    openDevice();
+//  } else if (shouldClose()) {
+//    closeDevice();
+//  }
 }
 
 double getInput() {
-  
-  double mVin = analogRead(MIC_IN)*4.9; // TODO: why is this 4.9? maybe Adam knows
-  
-  if (mVin <= 0)
+  gnd_ref = analogRead(GND);
+  double mVin = analogRead(MIC_IN);
+  if (mVin < 0) {
     Serial.println("Invalid read!!");
-    Serial.print(mVin);
-  // Serial.print("... mVin - Voff: ");
-  // Serial.println(mVin);
-  double dB = (20*log10(mVin/(S_total)));
-//  dB = 100;
-  return dB;
+  }
+//   Serial.println(gnd_ref);
+   Serial.println(mVin);
+  return mVin;
 }
 
-double testGetInput() {
-  int totalSecondsElapsed = (millis() - startTime) * timeAcceleration / 1000; // in seconds
+double getTestInput() {
+//  int totalSecondsElapsed = (millis() - startTime) * timeAcceleration / 1000; // in seconds
+  int vin = vinPrev + random(-2, 3);
+  if (vin < 0 || vin > 150) {
+    vin = 10;
+  }
+  vinPrev = vin;
+//  Serial.println(vin);
+  if (isClosed)
+    vin -= 2;
+  return vin;
+}
 
-  // call one of the test functions
-  double noiseLevel = stepper(totalSecondsElapsed);
+void testHBridge(){
+    Serial.println("Off");
+    digitalWrite(LEFT_BIT_1, LOW);
+    digitalWrite(LEFT_BIT_3, LOW);
+    delay(500);
 
-  if (isClosed) {
-    return noiseLevel - 15;
-  } else {
-    return noiseLevel;
-  }  
+    Serial.println("FWD");
+    digitalWrite(LEFT_BIT_1, HIGH);
+    digitalWrite(LEFT_BIT_3, LOW);
+    delay(500);
+
+    Serial.println("Off");
+    digitalWrite(LEFT_BIT_1, LOW);
+    digitalWrite(LEFT_BIT_3, HIGH);
+    delay(500);
+
+    Serial.println("BACK");
+    digitalWrite(LEFT_BIT_1, HIGH);
+    digitalWrite(LEFT_BIT_3, HIGH);
+    delay(500);
 }
 
 double V_to_dB(double vin) {
-  return 20 * log10(vin/vref);
+//  return 20 * log10(vin/vref);
+    return (20*log10(vin/(S_total)));
 }
 
 void recordDB(double dB) {
   int currentTime = millis();
-
   // Check if the current level is louder than the previous 
   if (dB > currentMaxDB) {
     currentMaxDB = dB;
   }
 
-  Serial.print("Elapsed time: ");
-  Serial.println(currentTime - lastResetTime);
+//  Serial.print("Elapsed time: ");
+//  Serial.println(currentTime - lastResetTime);
   if ((currentTime - lastResetTime) > WINDOW) {
-    Serial.println("##################### Next window #####################################");
+//    Serial.println("##################### Next window #####################################");
     lastResetTime = currentTime;
     storedPastDB[currentIndex] = (int)currentMaxDB;
     currentMaxDB = 0;
@@ -109,10 +137,9 @@ void recordDB(double dB) {
     }
   }
 
-  Serial.print("Current max db: ");
-  Serial.println(currentMaxDB);
-
-  delay(10);
+//  Serial.print("Current max db: ");
+//  Serial.println(currentMaxDB);
+//  delay(10);
 }
 
 
@@ -136,7 +163,6 @@ int shouldOpen() {
       return 0;
     }
   }
-
   if (currentMaxDB > LOWER_THRESHOLD) {
     return 0;
   }
@@ -146,19 +172,17 @@ int shouldOpen() {
 
 void openDevice() {
   Serial.println("Opening device");
+  digitalWrite(LEFT_BIT_1, HIGH);
+  digitalWrite(LEFT_BIT_3, LOW);
   isClosed = 0;
 }
 
 void closeDevice() {
   Serial.println("Closing device");
+  digitalWrite(LEFT_BIT_1, LOW);
+  digitalWrite(LEFT_BIT_3, HIGH);
   isClosed = 1;
 }
 
-double stepper(int secondsElapsed) {
-  if (secondsElapsed % 100) {
-    return 90; 
-  } else {
-    return 70;
-  }
-}
+// removed "stepper" function - Adam
 
